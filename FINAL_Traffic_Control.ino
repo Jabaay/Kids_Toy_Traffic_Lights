547237c677580a1e225ed2a31c166c734d71c9aa
Jabaay <119708810+Jabaay@users.noreply.github.com>
2024-05-17 17:50:57 -0400

    Update FINAL_Traffic_Control.ino

--- FINAL_Traffic_Control.ino
+++ FINAL_Traffic_Control.ino
@@ -7,12 +7,6 @@ const int greenLED = 4;
 const int redButton = 7;
 const int greenButton = 8;
 
-const int pedestrianButton = 9;
-const int pedestrianRedLED = 5;
-const int pedestrianGreenLED = 6;
-const int pedestrianRedWalkLED = 10;
-const int pedestrianGreenWalkLED = 11;
-
 // Define LCD properties
 const int rs = 12;
 const int en = 13;
@@ -34,15 +28,6 @@ void setup() {
   pinMode(redButton, INPUT_PULLUP);
   pinMode(greenButton, INPUT_PULLUP);
 
-  // Initialize button pin as input with pull-up resistor
-  pinMode(pedestrianButton, INPUT_PULLUP);
-
-  // Initialize LED pins as output
-  pinMode(pedestrianRedLED, OUTPUT);
-  pinMode(pedestrianGreenLED, OUTPUT);
-  pinMode(pedestrianRedWalkLED, OUTPUT);
-  pinMode(pedestrianGreenWalkLED, OUTPUT);
-
   // Initialize LCD
   lcd.begin(16, 2);
 
@@ -53,76 +38,49 @@ void setup() {
 void loop() {
   // Check if red button is pressed
   if (digitalRead(redButton) == LOW) {
-    // Activate vehicle traffic sequence
-    activateVehicleTraffic();
+    // Activate red traffic light
+    digitalWrite(greenLED, LOW);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(redLED, HIGH);
+    // Update LCD
+    lcdPrint("Cross");
+    delay(10000); // Red light duration: 10 seconds
   }
   
   // Check if green button is pressed
   if (digitalRead(greenButton) == LOW) {
-    // Activate pedestrian crossing sequence
-    activatePedestrianCrossing();
+    // Activate green traffic light
+    digitalWrite(redLED, LOW);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(greenLED, HIGH);
+    // Update LCD
+    lcdPrint("Do Not Cross");
+    delay(10000); // Green light duration: 10 seconds
   }
 
-  // Check if pedestrian button is pressed
-  if (digitalRead(pedestrianButton) == LOW) {
-    // Activate pedestrian crossing
-    pedestrianCrossing = true;
-    activatePedestrianCrossing();
-  } else {
-    // Pedestrian button not pressed, reset crossing status
-    pedestrianCrossing = false;
-    resetPedestrianCrossing();
+  // Automatic light switching if no buttons are pressed
+  if (digitalRead(redButton) == HIGH && digitalRead(greenButton) == HIGH) {
+    // Activate vehicle traffic sequence
+    activateVehicleTraffic();
   }
 }
 
 void activateVehicleTraffic() {
   // Vehicle traffic sequence
   digitalWrite(greenLED, HIGH);
-  delay(5000); // Green light duration: 5 seconds
+  lcdPrint("Do not Cross");
+  delay(10000); // Green light duration: 10 seconds
+  
   digitalWrite(greenLED, LOW);
-  
   digitalWrite(yellowLED, HIGH);
-  delay(2000); // Yellow light duration: 2 seconds
-  digitalWrite(yellowLED, LOW);
+  lcdPrint("Do Not Cross");
+  delay(3000); // Yellow light duration: 3 seconds
   
+  digitalWrite(yellowLED, LOW);
   digitalWrite(redLED, HIGH);
-  delay(5000); // Red light duration: 5 seconds
+  lcdPrint("Do not Cross");
+  delay(10000); // Red light duration: 10 seconds
   digitalWrite(redLED, LOW);
-}
-
-void activatePedestrianCrossing() {
-  // Display message on LCD
-  lcdPrint("Cross");
-
-  // Activate pedestrian red light and green walk light
-  digitalWrite(pedestrianRedLED, HIGH);
-  digitalWrite(pedestrianGreenWalkLED, HIGH);
-  delay(5000); // Pedestrian crossing duration: 5 seconds
-
-  // Flash pedestrian red walk LED
-  for (int i = 0; i < 5; i++) {
-    digitalWrite(pedestrianRedWalkLED, HIGH);
-    delay(500);
-    digitalWrite(pedestrianRedWalkLED, LOW);
-    delay(500);
-  }
-
-  // Turn off pedestrian green walk light
-  digitalWrite(pedestrianGreenWalkLED, LOW);
-
-  // Activate pedestrian green light
-  digitalWrite(pedestrianGreenLED, HIGH);
-}
-
-void resetPedestrianCrossing() {
-  // Display message on LCD
-  lcdPrint("No Cross");
-
-  // Turn off pedestrian lights
-  digitalWrite(pedestrianRedLED, LOW);
-  digitalWrite(pedestrianGreenLED, LOW);
-  digitalWrite(pedestrianRedWalkLED, LOW);
-  digitalWrite(pedestrianGreenWalkLED, LOW);
 }
 
 // Function to print a message on the LCD
